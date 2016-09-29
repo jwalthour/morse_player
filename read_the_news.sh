81d29680a6ff6efe1ba63393fbae8d6b2e453906
John Walthour <john.walthour@gmail.com>
2016-09-29 12:49:11 -0400

    Now with both Reuters and Google news

--- read_the_news.sh
+++ read_the_news.sh
@@ -1,11 +1,20 @@
 # Downloads a news RSS feed and reads out the headlines in morse code.
-SOURCE=http://feeds.reuters.com/Reuters/worldNews?format=xml
-curl $SOURCE 2>/dev/null \
-    | sed 's/></>\n</g' \
-    | grep '<title>' \
-    | sed  's/<title>\(.*\)<\/title>/\1/g' \
-    | sed "s/&apos;/'/g" \
-    | sed 's/Top Stories - Google News//g' \
-    | sed 's/Reuters News//g' \
-    | sed 's/Reuters: World News//g' \
-    | ipython morse_player.py -- -v 20 
+function readNews {
+    SOURCE=$1
+    echo Downloading $SOURCE
+    curl $SOURCE 2>/dev/null \
+        | sed 's/></>\n</g' \
+        | grep '<title>' \
+        | sed  's/<title>\(.*\)<\/title>/\1/g' \
+        | sed "s/&apos;/'/g" \
+        | sed 's/Top Stories - Google News//g' \
+        | sed 's/Reuters News//g' \
+        | sed 's/Reuters: World News//g' \
+        | ipython morse_player.py -- -v 20;
+}
+SOURCE0=http://feeds.reuters.com/Reuters/worldNews?format=xml
+SOURCE1=https://news.google.com/?output=rss
+while [ 1 ]; do
+    readNews $SOURCE0
+    readNews $SOURCE1
+done
