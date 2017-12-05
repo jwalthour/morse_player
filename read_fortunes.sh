10b6e73d8bd863a1fbc8f9e4e1c756ed7d245a0b
John Walthour <john.walthour@gmail.com>
2017-12-05 13:02:41 -0500

    lunchtime tweaks

--- read_fortunes.sh
+++ read_fortunes.sh
@@ -3,11 +3,16 @@ BASE_URL="http://anduin.eldar.org/cgi-bin/fortune.pl?text_format=yes&fortune_db=
 # DB_LIST="fortunes fortunes2 limerick pratchett startrek bofh-excuses futurama farber netbsd"
 DB_LIST="fortunes fortunes2 pratchett startrek bofh-excuses futurama farber netbsd"
 
+TONE_FREQ=220
+WORDS_PER_MIN=10
+VOLUME_PERCENT=30
+CHAR_GAP=7
+
 while true; do
 	for db in $DB_LIST; do
 		SOURCE="$BASE_URL$db"
 		echo "Using $db"
-		curl $SOURCE 2>/dev/null | cat | ipython morse_player.py 2>/dev/null
+		curl $SOURCE 2>/dev/null | cat | ipython morse_player.py -- -f $TONE_FREQ -w $WORDS_PER_MIN -v $VOLUME_PERCENT -g $CHAR_GAP 2>/dev/null
 		# curl $SOURCE 
 		# sleep 1
 		sleep 10
