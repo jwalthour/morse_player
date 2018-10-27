b479ec7e6bb5348970d1f86fb319a09becbeee5e
John Walthour <john.walthour@gmail.com>
2018-10-26 23:12:23 -0400

    Made a smaller trimmed variant of just the bell ringing sound for the "dah".  Now this starts to sound like morse code, although the variation between dit and dah is a little jarring.  It's trivial to tell them apart, which may make this a hair unfair.

--- ZenPlayer/ZenPlayer/MainWindow.xaml.cs
+++ ZenPlayer/ZenPlayer/MainWindow.xaml.cs
@@ -28,8 +28,8 @@ namespace ZenPlayer
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
-            System.IO.Stream ditStream = a.GetManifestResourceStream("ZenPlayer.ZenAudio.219159__jagadamba__frogblock03.wav");
-            System.IO.Stream dahStream = a.GetManifestResourceStream("ZenPlayer.ZenAudio.trimmed_346328__isteak__bright-tibetan-bell-ding-b-note-cleaner.wav");
+            System.IO.Stream ditStream = a.GetManifestResourceStream("ZenPlayer.ZenAudio.dit0_67ms.wav");
+            System.IO.Stream dahStream = a.GetManifestResourceStream("ZenPlayer.ZenAudio.dah_202ms.wav");
             System.Media.SoundPlayer ditPlayer = new System.Media.SoundPlayer(ditStream);
             System.Media.SoundPlayer dahPlayer = new System.Media.SoundPlayer(dahStream);
             await Task.Run(() => { ditPlayer.Load(); });
