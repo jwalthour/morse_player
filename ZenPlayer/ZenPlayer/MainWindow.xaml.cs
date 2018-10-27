5df912feb63fecabf8874932c528acaf1c1cf633
John Walthour <john.walthour@gmail.com>
2018-10-26 22:37:20 -0400

    Added all the wav files as embedded resources so we can have a more portable .exe file.  Can play them on button press.

--- ZenPlayer/ZenPlayer/MainWindow.xaml.cs
+++ ZenPlayer/ZenPlayer/MainWindow.xaml.cs
@@ -27,7 +27,11 @@ namespace ZenPlayer
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\spami\git\jwalthour\morse_player\ZenPlayer\ZenPlayer\ZenAudio\346328__isteak__bright-tibetan-bell-ding-b-note-cleaner.wav");
+
+            System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
+            //System.IO.Stream s = a.GetManifestResourceStream("ZenPlayer.ZenAudio.346328__isteak__bright-tibetan-bell-ding-b-note-cleaner.wav");
+            System.IO.Stream s = a.GetManifestResourceStream("ZenPlayer.ZenAudio.219159__jagadamba__frogblock03.wav");
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(s);
             player.Play();
         }
     }
