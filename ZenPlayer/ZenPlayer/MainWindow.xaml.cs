359f17d64f98aebed3d3fe1a435bb256afdd3331
John Walthour <john.walthour@gmail.com>
2018-10-26 23:00:46 -0400

    Trimmed the two dit files to have the same length.  Also made a trimmed version of a dah file to be 3x the dit file duration.  The dits sound like really convincing zen morse code, but the dah sounds too long and has too much of a loud percussion at the beginning.

--- ZenPlayer/ZenPlayer/MainWindow.xaml.cs
+++ ZenPlayer/ZenPlayer/MainWindow.xaml.cs
@@ -25,14 +25,41 @@ namespace ZenPlayer
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-
             System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
-            //System.IO.Stream s = a.GetManifestResourceStream("ZenPlayer.ZenAudio.346328__isteak__bright-tibetan-bell-ding-b-note-cleaner.wav");
-            System.IO.Stream s = a.GetManifestResourceStream("ZenPlayer.ZenAudio.219159__jagadamba__frogblock03.wav");
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer(s);
-            player.Play();
+            System.IO.Stream ditStream = a.GetManifestResourceStream("ZenPlayer.ZenAudio.219159__jagadamba__frogblock03.wav");
+            System.IO.Stream dahStream = a.GetManifestResourceStream("ZenPlayer.ZenAudio.trimmed_346328__isteak__bright-tibetan-bell-ding-b-note-cleaner.wav");
+            System.Media.SoundPlayer ditPlayer = new System.Media.SoundPlayer(ditStream);
+            System.Media.SoundPlayer dahPlayer = new System.Media.SoundPlayer(dahStream);
+            await Task.Run(() => { ditPlayer.Load(); });
+            await Task.Run(() => { dahPlayer.Load(); });
+
+            await Task.Run(() => { ditPlayer.PlaySync(); });
+            await Task.Delay(150);
+            await Task.Run(() => { ditPlayer.PlaySync(); });
+            await Task.Delay(150);
+            await Task.Run(() => { ditPlayer.PlaySync(); });
+            await Task.Delay(150);
+            await Task.Run(() => { ditPlayer.PlaySync(); });
+            await Task.Delay(150);
+
+            await Task.Delay(450);
+
+            await Task.Run(() => { dahPlayer.PlaySync(); });
+            await Task.Delay(150);
+            await Task.Run(() => { ditPlayer.PlaySync(); });
+            await Task.Delay(150);
+            await Task.Run(() => { dahPlayer.PlaySync(); });
+            await Task.Delay(150);
+            await Task.Run(() => { ditPlayer.PlaySync(); });
+            await Task.Delay(150);
+
         }
+
+        //private async void PlayChar(char c)
+        //{
+
+        //}
     }
 }
